f88468c2fd403b4467cb851b5743e005fb705268
rone.bento <rone.bento@vmarket.com.br>
2021-03-18 22:04:43 -0300

    Ta salvando no banco cloud :D

--- src/database/index.js
+++ src/database/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise
 
-mongoose.connect("mongodb://localhost/servercrud").then(() => {
+mongoose.connect("mongodb+srv://datawatchorg:dwosakaue@datawatchorg.4a9pt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority").then(() => {
     console.log("Conectado ao mongo")
 }).catch((err) => {
     console.log("Erro ao se conectar" + err)
