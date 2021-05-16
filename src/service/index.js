f68a17c10605f956c18d6b60161291f9449ad835
wesleyvs <wesleyvini.social@gmail.com>
2021-05-15 21:13:09 -0300

    Descriptografia da mensagem enviada pelo cetriolo-back

--- src/service/index.js
+++ src/service/index.js
@@ -1,16 +1,25 @@
 const amqp = require('amqplib/callback_api')
+const NodeRSA = require('node-rsa');
+var CryptoJS = require("crypto-js");
+var crypto = require('crypto');
+var path = require("path");
+var fs = require("fs")
+var aesjs = require("aes-js")
 const mongoose = require("mongoose") 
 require('../model/MonitoredData');
 require('../model/Main');
 const MonitoredData = mongoose.model("monitoredData")
 const Main = mongoose.model("main")
+var keySize = 256;
+var iterations = 100;
 
 let camposDadosPessoais = []
-const getCamposDadosPessoais = MonitoredData.find().lean().exec((err, result) => {
-    camposDadosPessoais = result.map(d => d.dado)
-})
+// const getCamposDadosPessoais = MonitoredData.find().lean().exec((err, result) => {
+//     camposDadosPessoais = result.map(d => d.dado)
+// })
 
 const rabbitUri = "amqps://cehwgqgh:bYyZ5ndnh6ZhHl1Gt8U9hQB4HEoCxGtz@owl.rmq.cloudamqp.com/cehwgqgh"
+var AESCrypt = {};
 
 amqp.connect(rabbitUri, function(error0, connection) {
     if (error0) {
@@ -27,10 +36,36 @@ amqp.connect(rabbitUri, function(error0, connection) {
         })
 
         console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue)
+
         channel.consume(queue, function (msg) {
             const payload = JSON.parse(msg.content.toString());
             let dictToSave ={};
-            console.log(payload);
+            console.log('\n\njson: \n' , payload);
+    
+            //Descriptografia da chave AES com a chave privada RSA
+            let chaveAESCript = payload.chaveAESCriptografadaRSA
+            let relativeOrAbsolutePathtoPrivateKey = 'assets/key.pem'
+            var absolutePath = path.resolve(relativeOrAbsolutePathtoPrivateKey);
+            var privateKey = fs.readFileSync(absolutePath, "utf8");
+            let key = new NodeRSA({b: 1024})
+            key.importKey(privateKey, 'pkcs8')
+            var decryptKeyAES = key.decrypt(chaveAESCript)
+
+            //Converte a chave AES de hexadecimal para byte
+            var encriptedBytesAES = aesjs.utils.hex.toBytes(decryptKeyAES.toString())
+
+            //Converte o IV de hexadecimal para byte
+            let iv = payload.iv
+            var encriptedBytesIV = aesjs.utils.hex.toBytes(iv.toString())
+
+            //Converte a mensagem criptografa com AES de hexadecimal para bytes
+            let mensagemCriptografadaAES = payload.mensagemCriptografadaAES
+            var encriptedBytesMessage = aesjs.utils.hex.toBytes(mensagemCriptografadaAES.toString())
+
+            //Decripta a mensagem em bytes criptografada com AES para texto comum (utf8)
+            var aescbc = new aesjs.ModeOfOperation.cbc(encriptedBytesAES, encriptedBytesIV)
+            var decriptedBytes = aescbc.decrypt(encriptedBytesMessage)
+            var message = aesjs.utils.utf8.fromBytes(decriptedBytes)
 
             const header = payload.header ? payload.header : {};
 
